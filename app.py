20eb8d1b5caa31af23ce86f7567f2d4c267358fe
AndresGarcia-15 <teherana@utb.edu.co>
2023-10-23 23:15:39 -0500

    Update Api challenge

--- app.py
+++ app.py
@@ -1,40 +1,31 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import json
-
-
+import uvicorn
 
 # Initializing the FastAPI app
 app = FastAPI()
 app.title = "DATOS CHALLENGE"
 
+origins = ['*']
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 # Carga el contenido del archivo JSON al iniciar la aplicación
-with open("data.json", "r") as json_file:
+with open("datos_filtrados_con_Rm_ind1.11.json", "r") as json_file:
     data_storage = json.load(json_file)
 
-# Ruta para obtener todos los datos
-
-@app.post("/upload/")
-async def upload_json(data: dict):
-    try:
-        # Agrega los datos JSON recibidos al almacenamiento
-        data_storage.append(data)
-        return {"message": "Datos JSON recibidos con éxito"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar los datos: {str(e)}")
-
+# Ruta para obtener todos los datos entre dos fechas
 @app.get("/data/")
-async def get_data():
-    # Devuelve todos los datos almacenados
-    return data_storage
+async def get_data(fecha_inicio: str, fecha_fin: str):
+    # Filtrar los datos que estén dentro del rango de fechas dado
+    filtered_data = [item for item in data_storage if fecha_inicio <= item.get("datetime") <= fecha_fin]
+    return filtered_data
 
-if __name__ == "__main__":
-    import uvicorn
+if __name__ == "__main":
     uvicorn.run(app, host="localhost", port=8000)
